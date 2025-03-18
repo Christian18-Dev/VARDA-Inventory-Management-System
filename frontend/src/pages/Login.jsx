ff49e95f57756e520d3f38246249e03d8fe487a1
Christian18-Dev <christianpaulrioja18@gmail.com>
2025-03-18 17:42:32 +0800

    Test fix for mobile

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -38,7 +38,7 @@ const LoginForm = () => {
   
       // 🎯 Log the login event with the correct role
       const logResponse = await fetch(
-        "http://localhost:10000/api/activitylogs/log",
+      `${import.meta.env.VITE_BACKEND_URL}/api/activitylogs/log`,
         {
           method: "POST",
           headers: {
