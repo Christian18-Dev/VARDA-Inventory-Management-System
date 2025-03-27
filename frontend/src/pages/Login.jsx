d61f215553031f2fb10cc0ebd6397642329ac514
christianpaulrioja18@gmail.com <christianpaulrioja18@gmail.com>
2025-03-27 13:57:29 +0800

    Import URL Fix

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/logoplaceholder.png";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const LoginForm = () => {
   const navigate = useNavigate();
