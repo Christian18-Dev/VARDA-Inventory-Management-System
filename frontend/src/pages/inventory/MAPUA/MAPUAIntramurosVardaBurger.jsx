64a5f24ba71955353df3198905bc8ff251fa49ac
christianpaulrioja18@gmail.com <christianpaulrioja18@gmail.com>
2025-04-04 15:56:47 +0800

    Added 0.5 sec Hover on Recent Activity | MAPUA/PUP/STJUDE Inventory Table

---
+++ frontend/src/pages/inventory/MAPUA/MAPUAIntramurosVardaBurger.jsx
@@ -0,0 +1,7 @@
+import BranchInventory from "../../../components/BranchInventory";
+
+const MAPUAIntramurosVardaBurgerInventory = () => {
+  return <BranchInventory branchName="MAPUA INTRAMUROS VARDA BURGER" />;
+};
+
+export default MAPUAIntramurosVardaBurgerInventory; 
