a604d136f053ea097054b6ba684f0eabd5def047
christianpaulrioja18@gmail.com <christianpaulrioja18@gmail.com>
2025-04-09 17:30:29 +0800

    Root Base Custom Domain Fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/VARDA-Inventory-Management-System/",
+  base: "/",
   server: {
     open: true,
     port: 3001,
