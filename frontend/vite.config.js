51114954933a5a7c663b8c664ccb19e087503ee1
Christian18-Dev <christianpaulrioja18@gmail.com>
2025-05-06 11:35:12 +0800

    404 refresh error fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/", // Change to '/your-repo-name/' if using a custom GitHub repo path
+  base: "/",
   server: {
     open: true,
     port: 3001,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  closeBundle: () => {
-    const indexPath = resolve(__dirname, 'dist/index.html');
-    const notFoundPath = resolve(__dirname, 'dist/404.html');
-    if (fs.existsSync(indexPath)) {
-      fs.copyFileSync(indexPath, notFoundPath);
-      console.log('✔ 404.html created for GitHub Pages SPA routing');
-    }
-  },
+  }
 })
